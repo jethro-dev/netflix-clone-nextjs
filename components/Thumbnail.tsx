c908e1a17d1c0a5152c073b54a32cb1996168a49
Jethro <galongau@gmail.com>
2022-07-16 03:56:52 +0100

    add skeleton thumbnail

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { IMG_SRC_URL } from "../utils/request";
 import { Movie } from "../typings";
@@ -9,17 +9,29 @@ interface Props {
 const FIXED_WIDTH: number = 180;
 
 const Thumbnail = ({ movie }: Props) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    movie && setIsLoading(false);
+  }, [movie]);
+
   return (
     <div
-      className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105`}
+      className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105 ${
+        isLoading && "bg-neutral-500 flex items-center justify-center"
+      }`}
     >
-      <Image
-        layout="fill"
-        className="rounded-sm object-cover"
-        src={`https://image.tmdb.org/t/p/w500${
-          movie.backdrop_path || movie.poster_path
-        }`}
-      />
+      {isLoading ? (
+        <h1>loading...</h1>
+      ) : (
+        <Image
+          layout="fill"
+          className="rounded-sm object-cover"
+          src={`https://image.tmdb.org/t/p/w500${
+            movie.backdrop_path || movie.poster_path
+          }`}
+        />
+      )}
     </div>
   );
 };
