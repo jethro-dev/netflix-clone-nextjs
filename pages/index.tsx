e349b906e3c7e98cc4ce634b6c807807d238f9ea
Jethro <galongau@gmail.com>
2022-07-16 01:09:50 +0100

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,16 @@ const Home: NextPage<Props> = ({
   romanceMovies,
   documentaries,
 }) => {
+  console.log({
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries,
+  });
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
