dfc885473c163f64e3f56378be8475dea4240665
Jethro <galongau@gmail.com>
2022-07-28 02:11:01 +0100

    update firebase config

--- firebase.tsx
+++ firebase.tsx
@@ -1,28 +1,25 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp, getApp, getApps } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-// import { getAuth } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBuu0YHXOcgmqWLZl-fiMn6lG2wUhdPg8k",
-//   authDomain: "next-firebase-stripe-39bf8.firebaseapp.com",
-//   databaseURL: "https://next-firebase-stripe-39bf8-default-rtdb.firebaseio.com",
-//   projectId: "next-firebase-stripe-39bf8",
-//   storageBucket: "next-firebase-stripe-39bf8.appspot.com",
-//   messagingSenderId: "777709922250",
-//   appId: "1:777709922250:web:4500ee09dca93e1406d133",
-// };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDM-5pKknVjJGioEgMESI7E4f3pp5PrMhQ",
+  authDomain: "jnetflix-app.firebaseapp.com",
+  projectId: "jnetflix-app",
+  storageBucket: "jnetflix-app.appspot.com",
+  messagingSenderId: "31810894617",
+  appId: "1:31810894617:web:b59cb42953c435cbc16388",
+};
 
-// // Initialize Firebase
-// const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-// const db = getFirestore();
-// const auth = getAuth();
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const auth = getAuth();
 
-// export default app;
-// export { auth, db };
-export {};
+export default app;
+export { auth, db };
