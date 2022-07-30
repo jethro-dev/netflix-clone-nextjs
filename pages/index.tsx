fd1953b79f0acc404229cae7e8f05e674e2b723a
Jethro <galongau@gmail.com>
2022-07-30 15:09:34 +0100

    add plans

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { Header, Banner, Row, Modal } from "../components";
+import { Header, Banner, Row, Modal, Plans } from "../components";
 import requests from "../utils/request";
 import { Movie } from "../typings";
 import useAuth from "../hooks/useAuth";
@@ -31,8 +31,13 @@ const Home: NextPage<Props> = ({
 }) => {
   const { logout, loading } = useAuth();
   const showModal = useRecoilValue(modalState);
+  const subscription = false;
 
-  if (loading) return null;
+  if (loading || subscription == null) return null;
+
+  if (!subscription) {
+    return <Plans />;
+  }
 
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
